32c22e48677cfb5b045a10ac5380686e4e27747d
cait <caitlin@madetech.com>
2021-02-24 15:32:56 +0000

    Add sunrise and sunset in result

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,7 +1,15 @@
 import React from "react";
 import classes from "./Conditions.module.css";
+// import Data from "../../data.json";
 
 const conditions = (props) => {
+  function dateFormat(time) {
+    return new Intl.DateTimeFormat("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(time);
+  }
   return (
     <div className={classes.Wrapper}>
       {props.error && (
@@ -20,7 +28,7 @@ const conditions = (props) => {
           />
           <p>
             It is currently {Math.round(props.responseObj.main.temp)} degrees
-            with {props.responseObj.weather[0].description}.
+            with {props.responseObj.weather[0].description}. Sunrise is at {dateFormat(props.responseObj.sys.sunrise)}. Sunset is at {dateFormat(props.responseObj.sys.sunset)}.
           </p>
         </div>
       ) : null}
